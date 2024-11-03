674b182a474296b046924c87fb2a82654eae90a6
SezginKahraman <sezgin.kahraman12@gmail.com>
2024-11-03 17:11:02 +0300

    categories made dynamic.

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("index", views.index),
     path("blogs", views.blogs, name="blogs"),
+    path("category/<slug:slug>", views.blogs_by_category, name="blogs_by_category"),
     # path("blogs/<int:id>", views.blog_details, name="blog_details"),
     path("blogs/<slug:slug>", views.blog_details, name="blog_details"),
 ]
--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-from blog.models import Blog
+from blog.models import Blog, Category
 
 # Create your views here.
 # def index(request):
@@ -54,12 +54,26 @@ dataJson = {
 
 def index(request):
     # all = Blog.objects.all()  # select * from blog
-    context = {"blogs": Blog.objects.filter(is_homepage=True, is_active=True)}
+    context = {
+        "blogs": Blog.objects.filter(is_homepage=True, is_active=True),
+        "categories": Category.objects.all(),
+    }
     return render(request, "blog/index.html", context)
 
 
 def blogs(request):
-    context = {"blogs": Blog.objects.filter(is_active=True)}
+    context = {
+        "blogs": Blog.objects.filter(is_active=True),
+        "categories": Category.objects.all(),
+    }
+    return render(request, "blog/blogs.html", context)
+
+
+def blogs_by_category(request, slug):
+    context = {
+        "blogs": Blog.objects.filter(slug=slug),
+        "categories": Category.objects.all(),
+    }
     return render(request, "blog/blogs.html", context)
 
 
